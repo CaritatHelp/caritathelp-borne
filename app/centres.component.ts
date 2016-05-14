70511c6fd71383c5621e70d6bbef2b8577336a99
Jérôme Caudoux <jerome.caudoux@epitech.eu>
2016-05-14 19:48:14 +0200

    [ Google Map ] Markers are working

--- app/centres.component.ts
+++ app/centres.component.ts
@@ -1,15 +1,12 @@
-import {Component, ElementRef} from "@angular/core";
-import {ANGULAR2_GOOGLE_MAPS_DIRECTIVES, ANGULAR2_GOOGLE_MAPS_PROVIDERS} from "angular2-google-maps/core";
+import {Component} from "@angular/core";
+import {ANGULAR2_GOOGLE_MAPS_DIRECTIVES, MouseEvent} from "angular2-google-maps/core";
+
 
 @Component({
 	selector: 'centres',
 	templateUrl: 'app/res/html/centres.component.html',
 	styleUrls: ['app/res/css/centres.component.css'],
-	directives: [ANGULAR2_GOOGLE_MAPS_DIRECTIVES], // this loads all angular2-google-maps directives in this component
-	// the following line sets the height of the map - Important: if you don't set a height, you won't see a map!!
-	providers: [
-		ANGULAR2_GOOGLE_MAPS_PROVIDERS
-	],
+	directives: [ANGULAR2_GOOGLE_MAPS_DIRECTIVES],
 	properties: ['height: height']
 })
 
@@ -17,11 +14,46 @@ export class CentresComponent {
 	lat: number = 51.678418;
 	lng: number = 7.809007;
 
-	private el:HTMLElement;
-	constructor(el: ElementRef) { this.el = el.nativeElement; }
+	clickedMarker(label: string, index: number) {
+		console.log(`clicked the marker: ${label || index}`)
+	}
+
+	mapClicked($event: MouseEvent) {
+		this.markers.push({
+			lat: $event.coords.lat,
+			lng: $event.coords.lng,
+			label: 'hclick'
+		});
+	}
+
+	markers: marker[] = [
+		{
+			lat: 51.673858,
+			lng: 7.815982,
+			label: 'A'
+		},
+		{
+			lat: 51.373858,
+			lng: 7.215982,
+			label: 'B'
+		},
+		{
+			lat: 51.723858,
+			lng: 7.895982,
+			label: 'C'
+		}
+	]
 
 	getGoogleMapHeight() {
 		var height = window.innerHeight - document.getElementById("app-header").offsetHeight;
 		return height + "px";
 	}
+}
+
+
+// just an interface for type safety.
+interface marker {
+	lat: number;
+	lng: number;
+	label?: string;
 }
--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,5 @@
-import { bootstrap }    from '@angular/platform-browser-dynamic';
-import { AppComponent } from './app.component';
-bootstrap(AppComponent);
+import {bootstrap} from "@angular/platform-browser-dynamic";
+import {AppComponent} from "./app.component";
+import {ANGULAR2_GOOGLE_MAPS_PROVIDERS} from "angular2-google-maps/core";
+
+bootstrap(AppComponent, ANGULAR2_GOOGLE_MAPS_PROVIDERS);
